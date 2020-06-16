397202b3ac4ca8167efe497379bacca3a95eebe8
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-06-16 11:08:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_dragable_card
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_dragable_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_dragable_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
